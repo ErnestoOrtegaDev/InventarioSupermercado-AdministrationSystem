f7f676f551f9fd7c7f6824870f8a35fb06642186
ErnestoOrtega4ABIS <ernestoortega1423@gmail.com>
2026-02-08 14:43:00 -0600

    feat: Axios Configuration

---
+++ client/src/api/axios.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+// Vite expone las variables en import.meta.env
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true, // Importante para envio/recibir Cookies
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+
+export default api;
