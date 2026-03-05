d2a9eae76438f4e1204cdfca3e0e0783d3b8ae4f
ErnestoOrtega4ABIS <ernestoortega1423@gmail.com>
2026-03-05 07:37:34 -0600

    feat: New Routes in AuthController for ForgetPassword

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { loginUser, logoutUser, registerUser, refreshToken, getProfile } from '../controllers/authController';
+import { loginUser, logoutUser, registerUser, refreshToken, getProfile, resetPassword, forgotPassword } from '../controllers/authController';
 import { protect } from '../middleware/authMiddleware';
 
 const router = express.Router();
@@ -9,5 +9,7 @@ router.post('/login', loginUser);
 router.post('/logout', logoutUser);
 router.post('/refresh', refreshToken);
 router.get('/profile', protect, getProfile);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
 
 export default router;
